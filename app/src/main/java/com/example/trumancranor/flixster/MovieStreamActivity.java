525b9c8bfc53b1310d2c2e31c8b3eebcf6c6487a
Truman Cranor <truman.cranor@airbnb.com>
2016-10-18 14:45:06 -0700

    App boilerplate

---
+++ app/src/main/java/com/example/trumancranor/flixster/MovieStreamActivity.java
@@ -0,0 +1,13 @@
+package com.example.trumancranor.flixster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_stream);
+    }
+}
